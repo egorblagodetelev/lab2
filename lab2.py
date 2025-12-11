b0567bd7a7fdbabb2cb07b1f45593f99b9c1cef8
egor <e1973433@gmail.com>
2025-12-11 21:06:02 +0300

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,42 @@
+import csv, random
+import xml.etree.ElementTree as ET
+
+with open('books.csv', encoding='cp1251') as file:
+    rows = list(csv.DictReader(file, delimiter=';'))
+
+title_key = 'Название'
+author_key = 'Автор'
+date_key = 'Дата поступления'
+
+
+count = sum(len(r[title_key]) > 30 for r in rows)
+print("ответ:", count)
+
+
+author = input("Введите ФИ автора:")
+filter = []
+for r in rows:
+    try:
+        year = int(r[date_key].split('.')[-1].split()[0])
+        if author in r[author_key] and 2016 <= year <= 2018:
+            filter.append(r)
+    except:
+        pass
+
+print('ответ:', len(filter))
+for r in filter[:3]:
+    print(r[author_key], r[title_key], r[date_key])
+
+
+bibs = random.sample(rows, 20)
+with open('bibliography.txt', 'w', encoding='utf-8') as f:
+    for i, b in enumerate(bibs, 1):
+        f.write(f"{i}. {b[author_key]}. {b[title_key]} - {b[date_key]}\n")
+print('файл создан')
+
+
+tree = ET.parse('currency.xml')
+root = tree.getroot()
+for val in root.findall('.//Valute'):
+    if int(val.find('Nominal').text) in (10, 100):
+        print(val.find('CharCode').text)
